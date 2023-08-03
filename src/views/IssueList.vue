b41c119d346ef3f63f40a020ebbe200bacbc05eb
yohayako <116733459+yohayako@users.noreply.github.com>
2023-08-03 09:11:42 +0900

    Add files via upload

---
+++ src/views/IssueList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>List</h1>
+            <el-button type="success" @click="getIssues()">issue取得</el-button>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'IssueList',
+    data(){
+        return {
+            issues:[]
+        }
+    },
+    methods: {
+        getIssues(){
+            axios.get('https://api.github.com/repos/yohayako/todo-app-project',
+                {
+                    headers:{
+                        'Accept': 'application/vnd.github.v3+json',
+                        'Content-Type':'application/json',
+                        },
+                },
+            )
+            .then((res)=>{
+                this.issues =res
+            })
+        }
+    }
+}
+</script>
+
