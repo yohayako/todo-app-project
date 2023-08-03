412d56ec05305cf359f2943096c520b6dc17bdb6
yohayako <116733459+yohayako@users.noreply.github.com>
2023-08-03 09:32:52 +0900

    Update IssueList.vue

--- src/views/IssueList.vue
+++ src/views/IssueList.vue
@@ -2,12 +2,32 @@
     <div>
         <h1>List</h1>
             <el-button type="success" @click="getIssues()">issue取得</el-button>
+            <el-row :gutter="12">
+                <el-col :span="12" v-for="issue in issues" :key="issue.id">
+                    <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
+                        <el-row :gutter="12">
+                            <el-col :span="21">{{ issue.title }}</el-col>
+                            <el-col :span="3">
+                                <el-button type="success" icon="el-icon-check" circle></el-button>
+                            </el-col>
+                        </el-row>
+                    </el-card>        
+                </el-col>
+            </el-row>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
 
+const client =axios.create({
+    baseURL:`${process.env.VUE_APP_GITHUB_ENDPOINT}`,
+    headers:{
+                'Accept': 'application/vnd.github.v3+json',
+                'Content-Type':'application/json',
+                'Authorization': `${process.env.VUE_APP_GITHUB_TOKEN}`
+            },
+})
 export default {
     name: 'IssueList',
     data(){
@@ -17,19 +37,11 @@ export default {
     },
     methods: {
         getIssues(){
-            axios.get('https://api.github.com/repos/yohayako/todo-app-project',
-                {
-                    headers:{
-                        'Accept': 'application/vnd.github.v3+json',
-                        'Content-Type':'application/json',
-                        },
-                },
-            )
+            client.get('/issues')
             .then((res)=>{
-                this.issues =res
+                this.issues=res.data;
             })
         }
     }
 }
 </script>
-
