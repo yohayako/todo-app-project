b14a7f0161189719ec2a51036d27e94c10caaeb6
yohayako <116733459+yohayako@users.noreply.github.com>
2023-08-08 08:55:05 +0900

    Update IssueList.vue

--- src/views/IssueList.vue
+++ src/views/IssueList.vue
@@ -1,47 +1,62 @@
 <template>
     <div>
-        <h1>List</h1>
-            <el-button type="success" @click="getIssues()">issue取得</el-button>
-            <el-row :gutter="12">
-                <el-col :span="12" v-for="issue in issues" :key="issue.id">
-                    <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
-                        <el-row :gutter="12">
-                            <el-col :span="21">{{ issue.title }}</el-col>
-                            <el-col :span="3">
-                                <el-button type="success" icon="el-icon-check" circle></el-button>
-                            </el-col>
-                        </el-row>
-                    </el-card>        
-                </el-col>
-            </el-row>
+      <h1>Todoリスト</h1>
+      <form @submit.prevent="addTodo()">
+        <el-input placeholder="todo" v-model="todo"></el-input>
+      </form>
+      <el-row :gutter="12">
+      <TodoItem v-for="( todo, index ) in todos" :key="index" @handleClick="removeTodo()" :todo="todo" :index="index"/>
+      <TodoItem v-for="( issue, index ) in issues" :key="issue.id" @handleClick="closeIssue()" :todo="issue.title" :index="index" />
+    </el-row>
     </div>
-</template>
-
-<script>
-import axios from 'axios';
-
-const client =axios.create({
-    baseURL:`${process.env.VUE_APP_GITHUB_ENDPOINT}`,
-    headers:{
-                'Accept': 'application/vnd.github.v3+json',
-                'Content-Type':'application/json',
-                'Authorization': `${process.env.VUE_APP_GITHUB_TOKEN}`
-            },
-})
-export default {
-    name: 'IssueList',
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  import TodoItem from '@/components/TodoItem.vue';
+  
+  const client = axios.create({
+    baseURL: `${process.env.VUE_APP_GITHUB_ENDPOINT}`,
+    headers: {
+      'Authorization': `token ${process.env.VUE_APP_GITHUB_TOKEN}`,
+      'Accept': 'application/vnd.github.v3+json',
+      'Content-Type':'application/json',
+    },
+  })
+  
+  export default {
+    name: 'TodosIssues',
+    components:{
+        TodoItem
+    },
     data(){
-        return {
-            issues:[]
+        return{
+        todo: '',
+        todos: [],
+        issues: []
         }
     },
     methods: {
-        getIssues(){
-            client.get('/issues')
-            .then((res)=>{
-                this.issues=res.data;
-            })
-        }
+        addTodo(){
+            this.todos.push(this.todo);
+            this.todo= '';
+        },
+        removeTodo(index){
+            this.todos.splice(index,1);
+        },
+      // ここからissueの管理
+      closeIssue(index){
+        this.issues.splice(index,1);
+      },
+      getIssues() {
+        client.get('issues')
+          .then((res) => {
+            this.issues = res.data
+        })
+      }
+    },
+    created() {
+      this.getIssues();
     }
-}
+  }
 </script>
